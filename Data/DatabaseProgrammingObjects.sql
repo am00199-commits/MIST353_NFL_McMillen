99a7a0333b198843724d2b74a46252ad3cc41590
Avery McMillen <am00199@mix.wvu.edu>
2026-02-26 12:46:34 -0500

    Did in class Query

--- Data/DatabaseProgrammingObjects.sql
+++ Data/DatabaseProgrammingObjects.sql
@@ -1,9 +1,33 @@
 use MIST353_NFL_RDB_McMillen;
+GO 
 
-select TeamName, TeamColors, Conference, Division   
-from Team t
-INNER JOIN ConferenceDivision c
-    ON t.ConferenceDivisionID = c.ConferenceDivisionID
-WHERE Conference = 'NFC'
-    and Division = 'North';
+CREATE or alter procedure procGetTeamsByConferenceDivision
+(   
+    @Conference NVARCHAR(50),
+    @Division NVARCHAR(50)
+)
+AS
+BEGIN
+    select TeamName, TeamColors, Conference, Division   
+    from Team t
+    INNER JOIN ConferenceDivision c
+        ON t.ConferenceDivisionID = c.ConferenceDivisionID
+    WHERE Conference = @Conference
+        and Division = @Division;
+END
+GO
+
+--EXECUTE procGetTeamsByConferenceDivision 'AFC', 'North';
+
+declare @myTeamName NVARCHAR(50) = 'Pittsburgh Steelers';    
+
+select OtherTeam.TeamName
+from Team MyTeam 
+inner join Team OtherTeam
+    on MyTeam.ConferenceDivisionID = OtherTeam.ConferenceDivisionID
+where MyTeam.TeamName = @myTeamName AND
+    OtherTeam.TeamName != @myTeamName;
+
+
+
 
